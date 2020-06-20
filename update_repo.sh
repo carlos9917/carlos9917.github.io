9542a034d7feb61ee87437d0b650ae2471e2ad7a
Carlos Peralta <carlos9917@gmail.com>
2020-06-20 04:10:03 +0000

    updte repo

---
+++ update_repo.sh
@@ -0,0 +1,7 @@
+cp ../log/current_state_sims.html .
+cp ../GanntChart_CARRA.html .
+cp ../waiting_times.html .
+cp ../quota_scripts/mars_east_plots.png .
+cp ../quota_scripts/mars_west_plots.png .
+git commit -am "updte repo"
+git push origin master
