9ec0d7cb0096fbad6e4aecf8f91841b7df802719
Carlos Peralta <carlos9917@gmail.com>
2020-07-21 14:02:23 +0000

    update repo

--- update_repo.sh
+++ update_repo.sh
@@ -3,5 +3,10 @@ cp ../GanntChart_CARRA.html .
 cp ../waiting_times.html .
 cp ../quota_scripts/mars_east_plots.png .
 cp ../quota_scripts/mars_west_plots.png .
+cp ../dayProgress_NE_1_2020.png ./figs
+cp ../dayProgress_NE_2_2020.png ./figs
+cp ../dayProgress_NE_3_2020.png ./figs
+cp ../dayProgress_IGB_2B_2020.png ./figs
+cp ../dayProgress_IGB_3_2020.png ./figs
 git commit -am "update repo"
 git push origin master
