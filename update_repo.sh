8da34d780c45f896dad72ff86ede02570eea8556
Carlos Peralta <carlos9917@gmail.com>
2020-08-24 14:38:10 +0000

    update repo

--- update_repo.sh
+++ update_repo.sh
@@ -5,6 +5,7 @@ cp ../quota_scripts/mars_east_plots.png .
 cp ../quota_scripts/mars_west_plots.png .
 cp ../dayProgress_IGB_1_2020.png ./figs
 cp ../dayProgress_IGB_2_2020.png ./figs
+cp ../dayProgress_IGB_1B_2020.png ./figs
 cp ../dayProgress_IGB_2B_2020.png ./figs
 cp ../dayProgress_NE_1_2020.png ./figs
 cp ../dayProgress_NE_2_2020.png ./figs
