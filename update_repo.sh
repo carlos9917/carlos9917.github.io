b152700f8f5205bb4687884efc3364f7cac894ee
Carlos Peralta <carlos9917@gmail.com>
2021-01-06 14:58:24 +0000

    update repo

--- update_repo.sh
+++ update_repo.sh
@@ -22,6 +22,7 @@ fi
 cp ../GanntChart_CARRA.html .
 cp ../waiting_times.html .
 cp ../quota_scripts/mars_east_plots.png .
+cp ../quota_scripts/MARS_archive_status.png .
 cp ../quota_scripts/mars_west_plots.png .
 cp ../dayProgress_IGB_1_2020.png ./figs
 cp ../dayProgress_IGB_2_2020.png ./figs
