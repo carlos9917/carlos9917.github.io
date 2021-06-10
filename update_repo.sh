5628785219498bf63b4b3b1ab9a250c616856b1e
Carlos Peralta <carlos9917@gmail.com>
2021-06-10 12:11:26 +0000

    update repo

--- update_repo.sh
+++ update_repo.sh
@@ -6,7 +6,7 @@ if [ -z $1 ]; then
 #logreport=/home/ms/dk/nhd/scripts/carra/log/current_state_sims.html
 # Changed 20210610
 logreport=/home/ms/dk/nhd/scripts/harmon/harmon/current_state_sims.html
-cp ../log/current_state_sims.html .
+cp $logreport .
 if [ $? -eq 0 ]; then
     echo Copy html OK
 else
